b9ca57a50ebc33a48f0fc192d094e4b25bb0232a
Lincoln <linnhtetaung467@gmail.com>
2026-03-16 07:16:59 -0600

    timezone update

--- timezones.py
+++ timezones.py
@@ -35,4 +35,10 @@ TIMEZONE_MAP = {
     "new york": "America/New_York",
     "los angeles": "America/Los_Angeles",
     "chicago": "America/Chicago",
+
+    #uk
+    "uk": "Europe/London",
+    "united kingdom": "Europe/London",
+    "england": "Europe/London",
+    "london": "Europe/London",
 }
